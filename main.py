4d0473f21e54a93e38e6f8e39f7e19cc7cd9cf57
Tomaso Ethan <62837924+Neonexus45@users.noreply.github.com>
2025-02-05 16:10:53 +0100

    rendu kata fizzbuzz

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 
+def fizzbuzz(i):
+    if i % 3 == 0 and i % 5 == 0:
+        print('FizzBuzz', end="")
+    elif i % 3 == 0:
+        print('Fizz', end="")
+    elif i % 5 == 0:
+        print('Buzz', end="")
+    else:
+        print(i)
+    stringified = str(i)
+    if "3" in stringified:
+        print('Fizz', end="")
+    if "5" in stringified:
+        print('Buzz', end="")
+
+    print()
+
 def main():
-    print('Hello, World!')
-    return 0
+    for i in range(1, 101):
+        fizzbuzz(i)
+
+    fizzbuzz(53)
+    fizzbuzz(35)
 
 if __name__ == '__main__':
     main()
