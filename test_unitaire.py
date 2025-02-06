23e145c3de44f63e895c5df65b0b0a9f6663a9a0
thelssa <thelmalum0@gmail.com>
2025-02-06 17:00:47 +0100

    update test unitaire

--- test_unitaire.py
+++ test_unitaire.py
@@ -3,8 +3,10 @@ from main import fizzbuzz
 
 def test_cases():
     fb_result = fizzbuzz()
-    assert fb_result[2] == "Fizz" 
+    assert fb_result[2] == "Fizz"  
     assert fb_result[4] == "Buzz"  
     assert fb_result[14] == "FizzBuzz"  
-    assert fb_result[98] == "Buzz"  
-    assert fb_result[0] == "1" 
+    assert fb_result[99] == "Buzz"  
+    assert fb_result[0] == "1"  
+    
+    
